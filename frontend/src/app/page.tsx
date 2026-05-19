c9b03dc212b57548229f7580970dcba049df8e60
Nadja Müller <n.mueller2@hm.edu>
2026-05-19 12:05:23 +0200

    issue 22: feat: add Button and Section ui component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Button from "../../components/ui/Button";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
           width={100}
           height={20}
           priority
+        />
+        <Button children="Test"
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
